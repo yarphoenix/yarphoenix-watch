1dbe0469fa0d59c1e8eb9c16a7d418194502f87b
yarphoenix <yarikhectatuct@gmail.com>
2026-06-08 23:33:09 +0300

    fix: capitalize typeLabel in MovieCard

--- src/components/PosterCard.jsx
+++ src/components/PosterCard.jsx
@@ -41,7 +41,7 @@ export function PosterCard({ film, onOpen, index }) {
           }}
         >
           <span>{film.year}</span>
-          <span>{typeLabel}</span>
+          <span style={{textTransform: "capitalize"}}>{typeLabel}</span>
         </span>
       </div>
     </button>
